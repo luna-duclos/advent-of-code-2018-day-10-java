7b2749184664c0a4093d4ad00b1d55b715af4116
Luna Duclos <luna.duclos@embark-studios.com>
2022-07-20 12:45:39 +0200

    Add the code to actually advance and print

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // Puzzle found at https://adventofcode.com/2018/day/10
         // Problem statement and starting state are found there
-        /*
-position=<-3,  6> velocity=< 2, -1>
-         */
-        Lightscape s = new Lightscape(new ArrayList<Point>(Arrays.asList(new Point[]{
+        Lightscape s = new Lightscape(new ArrayList<Point>(Arrays.asList(
                 new Point(new Vector2(9, 1), new Vector2(0, 2)),
                 new Point(new Vector2(7, 0), new Vector2(-1, 0)),
                 new Point(new Vector2(3, -2), new Vector2(-1, 1)),
@@ -39,7 +37,17 @@ position=<-3,  6> velocity=< 2, -1>
                 new Point(new Vector2(-6,  0), new Vector2(2,  0)),
                 new Point(new Vector2(5,  9), new Vector2(1, -2)),
                 new Point(new Vector2(14,  7), new Vector2(-2,  0)),
-                new Point(new Vector2(-3,  6), new Vector2(2, -1)),
-        })));
+                new Point(new Vector2(-3,  6), new Vector2(2, -1))
+        )));
+
+        // Setup an infite loop
+        // We don't want to go too fast that our user can't follow, so we'll wait each iteration for user input
+        s.print();
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            scanner.nextLine();
+            s.advance();
+            s.print();
+        }
     }
 }
